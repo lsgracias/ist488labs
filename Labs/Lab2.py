b24ab5e0564cca81c2f7d560b76058461dcf15df
Lauren Gracias <lgracias2022@gmail.com>
2026-01-28 18:12:18 +0000

    update_lab2

--- Labs/Lab2.py
+++ Labs/Lab2.py
@@ -11,7 +11,7 @@ def read_pdf(uploaded_file):
     return text
 
 # Show title and description.
-st.title("📄 Document question answering")
+st.title("📄 Lab 2 - Document question answering")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
